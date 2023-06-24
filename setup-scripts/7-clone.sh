f769d5d5612b4f61ae31ed0fa4bdb0747565fa0d
drmikecrowe <drmikecrowe@gmail.com>
2023-06-24 18:20:57 +0000

    removing expired token

--- setup-scripts/7-clone.sh
+++ setup-scripts/7-clone.sh
@@ -1,2 +1,2 @@
 mkdir -p "${MNT}"/etc
-git clone --depth 1 --branch main https://x-oauth-basic:4259efff799811cb73535b71b645d62c90601ca5@github.com/drmikecrowe/nixos-zfs-master "${MNT}"/etc/nixos
+git clone --depth 1 --branch main https://github.com/drmikecrowe/nixos-zfs-master "${MNT}"/etc/nixos
