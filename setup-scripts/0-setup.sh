80a149ecbd2a38983be15ad6212d1b49e86b3fe8
drmikecrowe <drmikecrowe@gmail.com>
2023-06-24 18:31:40 +0000

    adding missing pkgs

--- setup-scripts/0-setup.sh
+++ setup-scripts/0-setup.sh
@@ -1,2 +1,6 @@
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+
+if ! command -v git; then nix-env -f '<nixpkgs>' -iA git; fi
+if ! command -v jq;  then nix-env -f '<nixpkgs>' -iA jq; fi
+if ! command -v partprobe;  then nix-env -f '<nixpkgs>' -iA parted; fi
